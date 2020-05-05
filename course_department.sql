493f10c0e3b4e4cee4e667dda41abf2ee6f63c6f
孙敬垚 <49359899+tiketiskte@users.noreply.github.com>
2020-05-06 00:49:36 +0800

    Add files via upload

--- course_department.sql
+++ course_department.sql
@@ -35,6 +35,7 @@ CREATE TABLE `department` (
 
 LOCK TABLES `department` WRITE;
 /*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('001','信自学院'),('002','管理学院'),('003','机电学院'),('004','外文学院'),('005','法学院'),('006','理学院');
 /*!40000 ALTER TABLE `department` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-25 21:12:38
+-- Dump completed on 2020-05-05 21:34:13
